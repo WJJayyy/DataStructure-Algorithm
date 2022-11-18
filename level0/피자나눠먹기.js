9cc8c3f8004edf0ef828a3cfed97df2811bdbd30
WJJayyy <91gjstr@gmail.com>
2022-11-18 15:17:03 +0900

    피자나눠먹기

---
+++ level0/피자나눠먹기.js
@@ -0,0 +1,13 @@
+//1.
+function solution(n) {
+  if (n % 7 === 0) {
+    return n / 7;
+  } else {
+    return Math.ceil(n / 7);
+  }
+}
+
+//2.
+function solution(n) {
+  return Math.floor((n - 1) / 7) + 1;
+}
