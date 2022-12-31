c62dd719c76da7b1039fcc96faf488c50266942a
WJJayyy <91gjstr@gmail.com>
2022-12-31 20:11:48 +0900

    빈도수 세기, 애너그램

---
+++ etc/예제2.js
@@ -0,0 +1,31 @@
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  const lookup = {};
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  console.log(lookup);
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagram('aaz', 'zza'))
+// validAnagram('', '') // true
+// validAnagram('aaz', 'zza') // false
+// validAnagram('anagram', 'nagaram') // true
+// validAnagram("rat","car") // false) // false
+// validAnagram('awesome', 'awesom') // false
+// validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+// validAnagram('qwerty', 'qeywrt') // true
+// validAnagram('texttwisttime', 'timetwisttext') // true
