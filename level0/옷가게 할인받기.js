942feae3126a490aba513c7ee8fac0c12081562a
WJJayyy <91gjstr@gmail.com>
2023-01-03 20:27:26 +0900

    countUniqueValues 알고리즘

--- level0/옷가게 할인받기.js
+++ level0/옷가게 할인받기.js
@@ -11,4 +11,17 @@ function solution(price) {
     return Math.floor(price * 0.8);
   }
   return price;
+} 
+
+function solution(price) {
+  var answer = 0;
+  if(price >= 100000 && price < 300000){
+    return Math.floor (answer = price *0.95)
+  } 
+  if (price >= 300000 && price < 500000) {
+    return Math.floor( answer = price * 0.9)
+  }if (price >= 500000) {
+     return Math.floor (answer = price * 0.8)
+  }
+ return price // 10만원 이하일때 조건
 }
