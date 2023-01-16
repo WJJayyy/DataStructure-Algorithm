005b552b33c91206e37599b1a7b0b66da1eb4775
WJJayyy <91gjstr@gmail.com>
2023-01-16 20:24:57 +0900

    예제8_ Sliding Window - minSubArrayLen

---
+++ etc/예제8_Sliding Window - minSubArrayLen.js
@@ -0,0 +1,38 @@
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+// Examples:
+
+// Time Complexity - O(n)
+
+// Space Complexity - O(1)
+
+function minSubArrayLen(arr, val) {
+  var sumVal = arr[0];
+  var left = 0;
+  var right = 0;
+  var minLen = Infinity;
+
+  for (let i = 0; right < arr.length; i++) {
+    //크거나 같으면 왼쪽이동
+    if (sumVal >= val) {
+      sumVal -= arr[left];
+      minLen = Math.min(minLen, right - left + 1);
+      left++;
+    } //작으면 오른쪽이동
+    else {
+      right++;
+      sumVal += arr[right];
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
+
+minSubArrayLen([2, 3, 1, 2, 4, 3], 7); // 2 -> because [4,3] is the smallest subarray
+minSubArrayLen([2, 1, 6, 5, 4], 9); // 2 -> because [5,4] is the smallest subarray
+minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52); // 1 -> because [62] is greater than 52
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39); // 3
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55); // 5
+minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11); // 2
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95); // 0
