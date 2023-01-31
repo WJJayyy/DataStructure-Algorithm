906fc712161fd760009fed7f67d1c1bac144e6ac
WJJayyy <91gjstr@gmail.com>
2023-01-31 21:00:22 +0900

    재귀함수_예제12-productOfArray

---
+++ etc/예제12_productOfArray.js
@@ -0,0 +1,18 @@
+/*
+productOfArray
+Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+*/
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productOfArray(arr) {
+  return arr.reduce((a, b) => a * b, 1);
+}
+
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+  console.log(arr[0]);
+  return arr[0] * productOfArray(arr.slice(1));
+}
+console.log(productOfArray([1, 2, 3, 10]));
