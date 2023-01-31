03ed7e0fb2c0f151289204b8f2f2aa30516bc42c
WJJayyy <91gjstr@gmail.com>
2023-01-31 21:05:14 +0900

    재귀함수_예제13-recursiveRange

--- etc/예제12_productOfArray.js
+++ etc/예제12_productOfArray.js
@@ -6,13 +6,13 @@ Write a function called productOfArray which takes in an array of numbers and re
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
 
-function productOfArray(arr) {
+function productOfArray_(arr) {
   return arr.reduce((a, b) => a * b, 1);
 }
+console.log(productOfArray_([1, 2, 3, 10]));
 
 function productOfArray(arr) {
   if (arr.length === 0) return 1;
-  console.log(arr[0]);
   return arr[0] * productOfArray(arr.slice(1));
 }
 console.log(productOfArray([1, 2, 3, 10]));
