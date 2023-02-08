8fd55633d292704e2bc381c626580b806ee91cf9
WJJayyy <91gjstr@gmail.com>
2023-02-08 21:40:26 +0900

    [refactor] 삽입정렬

--- etc/삽입정렬.js
+++ etc/삽입정렬.js
@@ -1,7 +1,10 @@
 function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
+  let i, j;
+
+  for (i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
-    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = currentVal;
@@ -11,3 +14,19 @@ function insertionSort(arr) {
 }
 
 console.log(insertionSort([2, 1, 9, 76, 4]));
+
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    const checkValue = arr[i];
+    let left = i - 1;
+
+    while (left >= 0 && arr[left] > checkValue) {
+      arr[left + 1] = arr[left];
+      left--;
+    }
+    arr[left + 1] = checkValue;
+  }
+  return arr;
+};
+
+console.log(insertionSort([6, 3, 5, 1, 9, 4, 8, 2, 7]));
